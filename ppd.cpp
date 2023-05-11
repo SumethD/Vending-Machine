8f4c5ccc81eb37f48de2c67a1e4fc10b39591a5d
Sumeth Lokuliyana <sumethlokuliyana76@gmail.com>
2023-05-11 23:33:35 +1000

    purchase item works 70%

--- ppd.cpp
+++ ppd.cpp
@@ -17,20 +17,41 @@ std::string displayItems(LinkedList &stock);
 
 int main(int argc, char **argv)
 {
-    std::string choice;
+   std::string choice;
     if (argc != 3)
     {
-        std::cerr << "Use: " << argv[0] << " stock.dat coins.dat" << std::endl;
+        std::cerr << "Use: " << argv[0] << " stock coins" << std::endl;
         return 1;
     }
 
     std::string stockFile = argv[1];
     std::string coinsFile = argv[2];
+    bool incorrect_f = false;
     LinkedList itemstock;
     Coin coinstock;
 
     std::string filename = stockFile;
     std::string coinsf = coinsFile;
+
+    // Check if coins file has exactly 8 lines
+    std::ifstream coins(coinsf);
+    std::string line;
+    int count = 0;
+    while (std::getline(coins, line)) {
+        count++;
+    }
+    while (count != 8) {
+        std::cerr << "Coins file should have exactly 8 lines" << std::endl;
+        std::cout << "Please enter the correct coins file: ";
+        incorrect_f= true;
+        std::cin >> coinsf;
+        coins.open(coinsf);
+        count = 0;
+        while (std::getline(coins, line)) {
+            count++;
+        }
+        coins.close();
+    }
 
     std::string new_coinsfile = "new_coins.dat";
     std::string new_stockfile = "new_stock.dat";
@@ -39,7 +60,12 @@ int main(int argc, char **argv)
     itemstock.loadData(filename);
 
     itemstock.sortByName();
-    choice = mainMenu();
+    if (incorrect_f){
+        choice = mainMenu();
+        std::getline(std::cin, choice);
+    }else{
+        choice = mainMenu();
+    }
     bool exitLoop = false;
     while (!exitLoop) {
         // check if the input is empty (i.e., "Enter" was pressed)
@@ -47,44 +73,194 @@ int main(int argc, char **argv)
             exitLoop = true;
         }
         //displays item if 1 input
-        else if (choice == "1") {
-            std::cout << "TEST_1" << std::endl;
+        else if (choice == "1") { 
             std::string newd = displayItems(itemstock);
             choice = mainMenu();
         }
         else if (choice == "2") {
-            std::string item_id;
-            int cents,item_prc;
-            // int remainder =1;
+            bool menutype =true,to_menu = false,invalid=false;
+            Stock * itm =nullptr;
+            std::string item_id,final_c="",centz;
+            int cents,item_prc,five_c=0, ten_c=0,twenty_c=0,fifty_c=0,one_d=0,two_d=0,five_d=0,ten_d=0;
+            float int_item,final_total=0;
+            int remainder =1;
+            std::vector<int> change_vec;
             std::cout << "Purchase Item" << std::endl;
             std::cout << "--------------------" << std::endl;
             std::cout << "Please enter the id of the item you wish to purchase:" << std::endl;
             std::getline(std::cin, item_id);
+            while(!itemstock.get(item_id)){
+                    std::cout <<"The " << item_id << " is invalid, ENTER a different ID: " << std::endl;
+                    std::getline(std::cin, item_id);
+            };
             if(itemstock.get(item_id)){
                 Price prc= itemstock.getPrice(item_id);
-                Stock * itm = itemstock.getStock(item_id);
+                itm = itemstock.getStock(item_id);
                 std::string price_str = std::to_string(prc.dollars) + "." + prc.cents;
                 item_prc = std::stod(price_str) * 100;
                 std::cout << item_prc << std::endl;
                 std::cout << "You have selected "<< '"'<< itm->name << "-"<<itm->description <<'"'<<".This will cost you $"<< prc.dollars<<"."<<prc.cents<< std::endl;
                 std::cout << "Please hand over the money - type in the value of each note/coin in cents." << std::endl;
-                std::cin >> cents;
-                while(!coinstock.newCoin(cents)){
-                    
-                    std::cout <<"Error:"<< cents/100.0 <<  "$ is not a valid denomination of money. Please try again."<< std::endl;
-                    std::cin >> cents;
+                std::getline(std::cin, centz);
+                if (centz.empty()){
+                    to_menu= true;
+                }
+                else{
+                    while(!invalid){
+                        try{
+                        cents = std::stoi(centz);
+                        invalid= true;
+                        }catch(const std::invalid_argument& e){
+                            std::cout <<"Error wrong format of currecny"<< std::endl;
+                            std::getline(std::cin, centz);
+
+                        }
+                    }
+
+                    while(!coinstock.newCoin(cents)){
+                        std::cout <<"Error:"<< cents/100.0 <<  "$ is not a valid denomination of money. Please try again."<< std::endl;
+                        std::getline(std::cin, centz);
+                        invalid =false;
+                        while(!invalid){
+                            try{
+                            cents = std::stoi(centz);
+                            invalid= true;
+                            }catch(const std::invalid_argument& e){
+                                std::cout <<"Error wrong format of currecny"<< std::endl;
+                                std::getline(std::cin, centz);
+
+                            }
+                        }
     
-                };
-                return 0;
-                    
+                    };
+                    remainder = cents -item_prc;
+                    while(remainder !=0){
+                        if (remainder <0){
+                            item_prc = std::abs(remainder); 
+                            int_item = item_prc / 100.0;
+                            std::cout << "You still need to give $" << int_item << std::endl;
+                            std::getline(std::cin, centz);
+                            invalid =false;
+                            while(!invalid){
+                                try{
+                                cents = std::stoi(centz);
+                                invalid= true;
+                                }catch(const std::invalid_argument& e){
+                                    std::cout <<"Error wrong format of currecny"<< std::endl;
+                                    std::getline(std::cin, centz);
+
+                                }
+                            }
+                            while(!coinstock.newCoin(cents)){
+                                std::cout <<"Error:"<< cents/100.0 <<  "$ is not a valid denomination of money. Please try again."<< std::endl;
+                                std::getline(std::cin, centz);
+                                    invalid =false;
+                                    while(!invalid){
+                                    try{
+                                    cents = std::stoi(centz);
+                                    invalid= true;
+                                    }catch(const std::invalid_argument& e){
+                                        std::cout <<"Error wrong format of currecny"<< std::endl;
+                                        std::getline(std::cin, centz);
+
+                                }
+                            }
+                            };
+                            remainder = cents - item_prc;
+                            menutype =false;
+                        }
+                        else if(remainder >0){
+                            change_vec=coinstock.getChange(remainder);
+                            for (int element : change_vec) {
+                                if (element == 1000) {
+                                    ten_d += 1;
+                                    remainder-= 1000;
+                                } else if (element == 500) {
+                                    five_d += 1;
+                                    remainder-= 500;
+                                } else if (element == 200) {
+                                    two_d += 1;
+                                    remainder-= 200;
+                                } else if (element == 100) {
+                                    one_d += 1;
+                                    remainder-= 100;
+                                } else if (element == 50) {
+                                    fifty_c += 1;
+                                    remainder-= 50;
+                                } else if (element == 20) {
+                                    twenty_c += 1;
+                                    remainder-= 20;
+                                } else if (element == 10) {
+                                    ten_c += 1;
+                                    remainder-= 10;
+                                } else if (element == 5) {
+                                    five_c += 1;
+                                    remainder-= 5;
+                                }
+                            }
+                            for (int i = 0; i < ten_d; i++) {
+                                final_c += "$10 ";
+                                final_total+=1000;
+                            }
+                            for (int j = 0; j < five_d; j++) {
+                                final_c += "$5 ";
+                                final_total+=500;
+                            }
+
+                            for (int k = 0; k < two_d; k++) {
+                                final_c += "$2 ";
+                                final_total+=200;
+                            }
+
+                            for (int l = 0; l < one_d; l++) {
+                            final_c += "$1 ";
+                            final_total+=100;
+                            }
+
+                            for (int m = 0; m < fifty_c; m++) {
+                                final_c += "50c ";
+                                final_total+=50;
+                            }
+
+                            for (int n = 0; n < twenty_c; n++) {
+                                final_c += "20c ";
+                                final_total+=20;
+                        }
+
+                            for (int o = 0; o < ten_c; o++) {
+                                final_c += "10c ";
+                                final_total+=10;
+                            }
+
+                            for (int p = 0; p < five_c; p++) {
+                                final_c += "5c ";
+                                final_total+=5;
+                            }
+                            menutype =true;
+                    }
+                }
+                }
             }
+            if (!to_menu){
+                itm->on_hand --;
+                final_total = final_total / 100.0;
+                std::cout <<"Here is your "<<itm->name <<" and your change of $ "<<final_total<<": "<<final_c<< std::endl;
+                if (menutype){
+                    choice =mainMenu();
+                    std::getline(std::cin, choice);
+                    }else{
+                choice =mainMenu();
+                }
+            }else{
+                choice =mainMenu();
+            }
+                    
         }
 
         else if (choice == "3") {
-            std::cout << "TEST_3" << std::endl;
             itemstock.saveStock(new_stockfile);
             coinstock.storeCoins(new_coinsfile);
-            return 0;
+            exitLoop =true;
         }
 
         else if (choice == "4") {
